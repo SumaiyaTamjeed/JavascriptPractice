1548dae1136c1833bfa8afe1abbcb14a741d9650
SumaiyaTamjeed <sumaiya.tamjeed@gmail.com>
2016-11-10 16:23:24 +1100

    golf msg

--- practice.js
+++ practice.js
@@ -137,3 +137,51 @@ console.log(testSize(17));
 console.log(testSize(3));
 console.log(testSize(11));
 console.log(testSize(50));
+
+/*In the game of golf each hole has a par meaning the average number of strokes
+ a golfer is expected to make in order to sink the ball in a hole to complete
+ the play. Depending on how far above or below par your strokes are, there is
+ a different nickname.
+
+Your function will be passed par and strokes arguments. Return the correct
+string according to this table which lists the strokes in order of priority;
+ top (highest) to bottom (lowest):
+
+Strokes	Return
+1	"Hole-in-one!"
+<= par - 2	"Eagle"
+par - 1	"Birdie"
+par	"Par"
+par + 1	"Bogey"
+par + 2	"Double Bogey"
+>= par + 3	"Go Home!"
+par and strokes will always be numeric and positive.*/
+
+
+function golfScore(par, strokes) {
+
+  if(strokes>=(par+3))
+    return "Go Home!";
+  else if (strokes==(par+2))
+    return"Double Bogey";
+  else if (strokes==(par+1))
+    return "Bogey";
+  else if(strokes==1)
+     return "Hole-in-one!";
+  else if(strokes==par)
+    return "Par";
+  else if(strokes==(par-1))
+    return "Birdie";
+  else if (strokes<=(par-2))
+    return "Eagle";
+   else if(strokes==1)
+     return "Hole-in-one!";
+
+
+  return "Golf";
+
+}
+
+// Change these values to test
+console.log(golfScore(4, 1));
+console.log(golfScore(4, 2));
